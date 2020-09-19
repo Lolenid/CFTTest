6ec797ab7bb2b400082ac644b9d65acbd5653ac3
Lolenid <THeSyi3wZBKS2sG>
2020-09-19 23:40:11 +0700

    added fragment ValuteList and action on it. Added pojo class Valute

--- app/src/main/java/com/example/cfttest/MainActivity.java
+++ app/src/main/java/com/example/cfttest/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.cfttest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+private NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(navController.getGraph()).build();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        NavigationUI.setupWithNavController(
+                toolbar, navController, appBarConfiguration);
+    }
+
+    public void onClickLeftButton(View view) {
+        navController.navigate(R.id.action_convertor_to_valuteList);
     }
 }
---
+++ app/src/main/java/com/example/cfttest/Valute.java
@@ -0,0 +1,67 @@
+package com.example.cfttest;
+
+public class Valute {
+    private String id;
+    private String numCode;
+    private String charCode;
+    private int nominal;
+    private String name;
+    private double value;
+
+    public Valute(String id, String numCode, String charCode, int nominal, String name, double value) {
+        this.id = id;
+        this.numCode = numCode;
+        this.charCode = charCode;
+        this.nominal = nominal;
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNumCode() {
+        return numCode;
+    }
+
+    public void setNumCode(String numCode) {
+        this.numCode = numCode;
+    }
+
+    public String getCharCode() {
+        return charCode;
+    }
+
+    public void setCharCode(String charCode) {
+        this.charCode = charCode;
+    }
+
+    public int getNominal() {
+        return nominal;
+    }
+
+    public void setNominal(int nominal) {
+        this.nominal = nominal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
